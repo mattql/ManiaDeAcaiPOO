9b274e46b53ecb2ba7663900b9f2ed521874520e
HugoGomes35 <88948975+HugoGomes35@users.noreply.github.com>
2021-08-16 14:33:28 -0300

    Update AdministradorVO.java

--- ManiaDeAcaiPOO/src/modelVO/AdministradorVO.java
+++ ManiaDeAcaiPOO/src/modelVO/AdministradorVO.java
@@ -1,31 +1,5 @@
 package modelVO;
 
 public class AdministradorVO {
-		private String loginADM;
-		private String senhaADM;
 		
-		//Login do ADM
-		public String getloginADM() {
-			return loginADM;
-		}
-		public String setloginADM(String loginADM) {
-			if (loginADM == null){
-			}
-			else {
-				if(loginADM.equals("seuze")) {
-					
-				}
-				else
-					this.loginADM = loginADM;
-			}
-			return loginADM;
-		}
-		
-		//Senha do ADM
-		public String getsenhaADM() {
-			return loginADM;
-		}
-		public String setsenhaADM(String senhaADM) {
-			return this.senhaADM = senhaADM;
-		}
 }
