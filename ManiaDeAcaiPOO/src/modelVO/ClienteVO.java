6b909b07857104d01f413f8c8d0ba9a409a9a948
hugogomes35 <hugo3694_@hotmail.com>
2021-08-16 15:49:07 -0300

    Codigo do ClienteVO

--- ManiaDeAcaiPOO/src/modelVO/ClienteVO.java
+++ ManiaDeAcaiPOO/src/modelVO/ClienteVO.java
@@ -1,5 +1,30 @@
 package modelVO;
 
 public class ClienteVO {
-
+	private String nomeCliente;
+	private String telefoneCliente;
+	
+	//Nome do Cliente.
+	public String getnomeCliente() {
+		return nomeCliente;
+	}
+	public void setnomecliente(String nomeCliente) {
+		if (nomeCliente.isEmpty()) {
+			System.out.println("Nome inválido");
+		}
+		else
+			this.nomeCliente = nomeCliente;
+	}
+	
+	//Telefone do Cliente.
+	public String gettelefoneCliente() {
+		return telefoneCliente;
+	}
+	public void setnomeCliente(String nomeCliente) {
+		if (nomeCliente.isEmpty()) {
+			System.out.println("Telefone Inválido");
+		}
+		else 
+			this.nomeCliente = nomeCliente;
+	}
 }
