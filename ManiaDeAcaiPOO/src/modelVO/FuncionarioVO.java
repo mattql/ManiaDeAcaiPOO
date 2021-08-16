eedebd6d4642140849d199756bf92184c6d47ca3
mattql <matheusqlacerda@hotmail.com>
2021-08-16 12:17:14 -0300

    Classe FuncionarioVO editada

--- ManiaDeAcaiPOO/src/modelVO/FuncionarioVO.java
+++ ManiaDeAcaiPOO/src/modelVO/FuncionarioVO.java
@@ -1,5 +1,35 @@
 package modelVO;
 
 public class FuncionarioVO {
-
+	private String nomeFuncionario;
+	private String telefoneFuncionario;
+	
+	
+	
+	
+	//Nome do Funcionário
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		if(nomeFuncionario.isEmpty()) {
+			System.out.println("Nome inválido.");
+		}
+		else
+			this.nomeFuncionario = nomeFuncionario;
+	}
+	
+	
+	//Telefone do Funcionário
+	public String getTelefoneFuncionario() {
+		return telefoneFuncionario;
+	}
+	public void setTelefoneFuncionario(String telefoneFuncionario) {
+		if(telefoneFuncionario.isEmpty()) {
+			System.out.println("Telefone inválido.");
+		}
+		else
+			this.telefoneFuncionario = telefoneFuncionario;
+	}
+	
 }
