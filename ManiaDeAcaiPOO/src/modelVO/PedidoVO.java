107dcd2423ec9e196d876c7d099a7c55ed5945b0
HugoGomes35 <88948975+HugoGomes35@users.noreply.github.com>
2021-08-17 08:04:06 -0300

    Update PedidoVO.java

--- ManiaDeAcaiPOO/src/modelVO/PedidoVO.java
+++ ManiaDeAcaiPOO/src/modelVO/PedidoVO.java
@@ -1,5 +1,15 @@
 package modelVO;
 
 public class PedidoVO {
+  private Date dataPedido;
+  
+  //Data do Pedido
+	public Date getDataPedido() {
+		return dataPedido;
+	}
+	public void setDataPedido(Date dataPedido) {
+		dataPedido = new Date();
+		this.dataPedido = dataPedido;
+	}
 
 }
