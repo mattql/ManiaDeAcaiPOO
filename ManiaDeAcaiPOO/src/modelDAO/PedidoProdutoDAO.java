71c71e629163e13ac7e52a7a9bde805c38bfff39
mattql <matheusqlacerda@hotmail.com>
2021-09-30 12:34:40 -0300

    Correção da classe PedidoProdutoDAO. Feito por Hugo e Matheus no Discord

--- ManiaDeAcaiPOO/src/modelDAO/PedidoProdutoDAO.java
+++ ManiaDeAcaiPOO/src/modelDAO/PedidoProdutoDAO.java
@@ -8,15 +8,13 @@ import java.util.List;
 import modelVO.PedidoProdutoVO;
 import modelVO.PedidoVO;
 
-public class PedidoProdutoDAO extends BaseDAO{
-	
+public class PedidoProdutoDAO extends BaseDAO<PedidoProdutoVO>{
 	public void inserir(PedidoProdutoVO[] pp) {
-		conn = getConnection();
 		String sql = "insert into pedido_produto (cod_pedidoproduto, cod_pedido, cod_produto) values (?, ?, ?)";
 		PreparedStatement ptst;
 		try {
 			for(int i = 0; i < pp.length; i++) {
-			ptst = conn.prepareStatement(sql);
+			ptst = getConnection().prepareStatement(sql);
 			ptst.setInt(1, pp[i].getCodPedidoProduto());
 			ptst.setInt(2, pp[i].getCod_pedido().getIdPedido());
 			ptst.setInt(3, pp[i].getCod_produto().getIdProduto());
@@ -28,12 +26,11 @@ public class PedidoProdutoDAO extends BaseDAO{
 	}
 	
 	public void remover(PedidoProdutoVO[] pp) {
-		conn = getConnection();
 		String sql = "delete from PedidoProduto where cod_pedidoproduto = ?";
 		PreparedStatement ptst;
 		try {
 			for(int i = 0; i < pp.length; i++) {
-			ptst = conn.prepareStatement(sql);
+			ptst = getConnection().prepareStatement(sql);
 			ptst.setInt(1, pp[i].getCodPedidoProduto());
 			ptst.executeUpdate();
 			}
@@ -43,13 +40,12 @@ public class PedidoProdutoDAO extends BaseDAO{
 	}
 	
 	public List<PedidoProdutoVO> listar(PedidoVO pedido) {
-		conn = getConnection();
 		String sql = "select * from PedidoProduto where codPedido = ?";
 		PreparedStatement ptst;
 		ResultSet rs;
 		List<PedidoProdutoVO> pedidoproduto = new ArrayList<PedidoProdutoVO>();
 		try {
-			ptst = conn.prepareStatement(sql);
+			ptst = getConnection().prepareStatement(sql);
 			ptst.setInt(1, pedido.getIdPedido());
 			rs = ptst.executeQuery(sql);
 			while(rs.next()) {
@@ -64,6 +60,38 @@ public class PedidoProdutoDAO extends BaseDAO{
 		}
 		return pedidoproduto;		
 	}
+
+	@Override
+	public void inserir(PedidoProdutoVO vo) {
+		//Método não utilizado
+	}
+
+	@Override
+	public void remover(PedidoProdutoVO vo) {
+		//Método não utilizado	
+	}
+
+	@Override
+	public void editar(PedidoProdutoVO vo) {
+		//Método não utilizado
+	}
+
+	@Override
+	public List<PedidoProdutoVO> listar() {
+		//Método não utilizado
+		return null;
+	}
+
+	@Override
+	public ResultSet pesquisarPorID(PedidoProdutoVO vo) {
+		//Método não utilizado
+		return null;
+	}
+
+	@Override
+	public ResultSet pesquisarPorNome(PedidoProdutoVO vo) {
+		//Método não utilizado
+		return null;
+	}
 	
 }
-
