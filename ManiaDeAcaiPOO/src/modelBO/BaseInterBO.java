d8107711577be3644f9f4a52c669d0dcaa97652a
mattql <matheusqlacerda@hotmail.com>
2021-10-01 13:22:59 -0300

    Mais uma pequena correção

--- ManiaDeAcaiPOO/src/modelBO/BaseInterBO.java
+++ ManiaDeAcaiPOO/src/modelBO/BaseInterBO.java
@@ -4,7 +4,6 @@ public interface BaseInterBO<VO> {
 	public void cadastrar(VO vo) throws Exception;
 	public void alterar(VO vo) throws Exception;
 	public void remover(VO vo) throws Exception;
-	public void editar(VO vo) throws Exception;
 	public void buscar() throws Exception;
 	public void buscarPorID(VO vo) throws Exception;
 	public void buscarPorNome(VO vo) throws Exception;
